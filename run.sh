a601653b06bbea69376a3e4f9b1071fee38c2952
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-07-02 22:55:07 +0900

    feat: Heap size 조절 추가

--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run --name=elastic-korean -d -p 9200:9200 -p 9300:9300 -v "$PWD/config":/usr/share/elasticsearch/config -v "$PWD/esdata":/usr/share/elasticsearch/data kyoungrok/elastic-korean
---
+++ scripts/run.sh
@@ -0,0 +1,1 @@
+docker run --name=elastic-korean -d -p 9200:9200 -p 9300:9300 -v "$PWD/config":/usr/share/elasticsearch/config -v "$PWD/esdata":/usr/share/elasticsearch/data --memory="4g" -e ES_HEAP_SIZE=2g kyoungrok/elastic-korean
