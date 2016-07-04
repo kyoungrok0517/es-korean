b33416a5e6c4da162793ba8513af4edc5d3a4b07
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2016-07-04 12:44:03 +0900

    feat: backup.sh 리눅스에서는 동작하는 것 확인

--- scripts/insert.sh
+++ scripts/insert.sh
@@ -4,30 +4,30 @@ ES='http://localhost:9200'
 ESIDX='seunjeon-idx'
 TYPE='test'
 
-curl -XDELETE "${ES}/${ESIDX}?pretty"
-sleep 1
-curl -XPUT "${ES}/${ESIDX}/?pretty" -d '{
-  "settings" : {
-    "index":{
-      "analysis":{
-        "analyzer":{
-          "korean":{
-            "type":"custom",
-            "tokenizer":"seunjeon_default_tokenizer"
-          }
-        },
-        "tokenizer": {
-          "seunjeon_default_tokenizer": {
-            "type": "seunjeon_tokenizer",
-            "user_words": ["낄끼빠빠,-100", "버카충", "abc마트"]
-          }
-        }
-      }
-    }
-  }
-}'
+# curl -XDELETE "${ES}/${ESIDX}?pretty"
+# sleep 1
+# curl -XPUT "${ES}/${ESIDX}/?pretty" -d '{
+#   "settings" : {
+#     "index":{
+#       "analysis":{
+#         "analyzer":{
+#           "korean":{
+#             "type":"custom",
+#             "tokenizer":"seunjeon_default_tokenizer"
+#           }
+#         },
+#         "tokenizer": {
+#           "seunjeon_default_tokenizer": {
+#             "type": "seunjeon_tokenizer",
+#             "user_words": ["낄끼빠빠,-100", "버카충", "abc마트"]
+#           }
+#         }
+#       }
+#     }
+#   }
+# }'
 
-sleep 1
+# sleep 1
 
 curl -XPOST "${ES}/${ESIDX}/${TYPE}" -d '{
   "title": "테스트1",
