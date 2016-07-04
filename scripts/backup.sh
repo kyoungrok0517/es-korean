e6701890676b901bc253a3ac17c6af87aee4fd4e
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-07-04 19:28:28 +0900

    chore(): backup.sh에 샘플 코드 추가

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,1 +1,5 @@
  docker run --rm --volumes-from esdata -v $(pwd):/backup busybox tar cvf /backup/backup.tar /usr/share/elasticsearch
+
+# docker run --rm --volumes-from dbstore -v $(pwd):/backup ubuntu tar cvf /backup/backup.tar /dbdata
+# docker run -v /dbdata --name dbstore2 ubuntu /bin/bash
+# docker run --rm --volumes-from dbstore2 -v $(pwd):/backup ubuntu bash -c "cd /dbdata && tar xvf /backup/backup.tar --strip 1"
